ec9822198c6a567039520d191ffe654c890db003
YashMahale06 <yashmahale2002@gmail.com>
2022-08-23 19:10:59 +0530

    Stack using array

---
+++ StackArray.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+using namespace std;
+
+struct Stack
+{
+    int size;
+    int top;
+    int *S;
+};
+void create(struct Stack *st)
+{
+    cout<<"Enter Size";
+    cin>>st->size;
+    st->top = -1;
+    st->S = new int[st->size];
+    
+};
+
+void Display(struct Stack *st)
+{
+    int i;
+
+    for(int i=st->top;i>=0;i--)
+    {
+        cout<<st->S[i]<<"\n";
+    }
+}
+
+void push(struct Stack *st, int x)
+{
+    if(st->top == st->size-1)
+    {
+        cout<<"Overflow"<<"\n";
+
+    }
+    else{
+        st->top++;
+        st->S[st->top] = x;
+    }
+}
+
+int pop(struct Stack *st)
+{
+    int x=-1;
+    if(st->top == -1)
+    {
+        cout<<"Underflow"<<"\n";
+
+    }
+    else{
+
+        x=st->S[st->top--];
+    }
+    return x;
+}
+int peek(struct Stack *st,int index)
+{
+    int x=-1;
+    if(st->top-index+1<0)
+    {
+        cout<<"Invalid Index"<<"\n";
+
+    }
+    x=st->S[st->top-index+1];
+    return x;
+}
+
+int isEmpty(struct Stack *st)
+{
+    if(st->top == -1)
+    {
+        return -1;
+
+    }
+    return 0;
+}
+int isFull(struct Stack *st)
+{
+    return st->top==st->size-1;
+}
+int StackTop(struct Stack *st)
+{
+    if(!isEmpty(st))
+    {
+        return st->S[st->top];
+    }
+    return -1;
+}
+int main()
+{
+    struct Stack st;
+
+    create(&st);
+    push(&st,10);
+    push(&st,20);
+    push(&st,30);
+    push(&st,40);
+    push(&st,50);
+    cout<<"peek element : "<<peek(&st,1)<<"\n";
+    cout<<"poped element : "<<pop(&st)<<"\n";
+    Display(&st);
+    return 0;
+}
